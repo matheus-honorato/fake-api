3e32b5cd347db70c8eee24d1c9439d639ad4b103
Matheus Honorato <matheuswebmw@gmail.com>
2024-11-12 00:20:49 -0300

    mudanças na  class tratamento dados

--- src/main.py
+++ src/main.py
@@ -20,14 +20,12 @@ class Tratamento_dados:
             logging.info(f"Tabela '{nome_tabela}' carregada no DuckDB.")
 
     def query_data(self, sql_query):
-        result_df = self.con.execute(sql_query).fetchdf()
-        return result_df
+        result_df = self.con.execute(f"COPY ({sql_query}) TO 'output.json' (FORMAT 'json')")
 
 class Api:
 
     def __init__(self, url):
         self.url = url
-        #self.download_folder = download_folder
 
     def request(self, endpoint):
         """Método para realizar uma requisição GET utilizando endpoint e com tratamento de erros das requisições."""
@@ -65,7 +63,63 @@ if __name__ == "__main__":
     load = tratamento.load_data()
     
     sql_query = '''
- 
+    WITH
+    users as (
+        SELECT id userId FROM table_0
+    ),
+    carts as (
+    SELECT 
+        id as id_cart, 
+        userId,
+        date,
+        unnest(products) AS product,
+        product['productId'] AS productId,
+        product['quantity'] AS product_Quantity
+    FROM table_1),
+    products as (
+        SELECT id as productId, category FROM table_2
+    ),
+    join_1 as (
+        SELECT 
+            a.userId, 
+            b.id_cart, 
+            productId,
+            CAST(b.date AS DATE) as data_carrinho,
+            product_Quantity
+        FROM users as a INNER JOIN carts as b 
+        ON a.userId = b.userId
+    ),
+    join_final as (
+    SELECT 
+        a.*, 
+        b.category 
+    FROM join_1 as a INNER JOIN products as b 
+    ON a.productId = b.productId),
+
+    max_dates AS (
+        SELECT 
+            userId, 
+            MAX(data_carrinho) AS data_carrinho
+        FROM join_final
+        GROUP BY userId
+    ),
+    rank_1 as (
+        SELECT 
+            userId, 
+            category, 
+            SUM(product_Quantity) product_Quantity
+        FROM join_final
+        GROUP BY ALL
+    ),
+    rank_2 as (
+    SELECT 
+        userId, category, 
+        product_Quantity, ROW_NUMBER() OVER (PARTITION BY userId ORDER BY product_Quantity desc) rnk
+    FROM rank_1)
+
+    SELECT a.userId, b.data_carrinho , category FROM rank_2 as a
+    JOIN max_dates as b ON a.userId = b.userId
+    WHERE rnk = 1
     '''
 
     result_df = tratamento.query_data(sql_query)
