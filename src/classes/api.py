dd7753228971801542bdb8d1f10752c27453d3aa
Matheus Honorato <matheuswebmw@gmail.com>
2024-11-12 22:51:37 -0300

    Adicionado docstring

--- src/classes/api.py
+++ src/classes/api.py
@@ -5,20 +5,30 @@ import logging
 class Api:
 
     def __init__(self, url):
+        """
+        Inicializa a classe Api com a URL base.
+
+        :param url: URL base da API.
+        """
         self.url = url
 
     def request(self, endpoint):
-        """Método para realizar uma requisição GET utilizando endpoint e com tratamento de erros das requisições."""
+        """
+        Realiza uma requisição GET para o endpoint e trata erros de requisição.
+
+        :param endpoint: O endpoint da API para onde a requisição será enviada.
+        :return: Resposta em formato JSON ou None em caso de erro.
+        """
         url = f"{self.url}/{endpoint}"
         try:
             logging.info(f"Realizando requisição para o endpoint: {url}")
-            response = requests.get(url, params=None)
+            response = requests.get(url)
             response.raise_for_status()
             logging.info(f"Requisição para o endpoint {url} foi bem-sucedida.")
             return response.json()
-        
         except requests.exceptions.RequestException as e:
             logging.error(f"Erro na requisição {url}: {e}")
+            return None
 
     def users(self):
         """ Obtém a lista de usuários da api """
--- src/classes/tratamento_dados.py
+++ src/classes/tratamento_dados.py
@@ -8,10 +8,19 @@ logging.basicConfig(level=logging.INFO)
 class Tratamento_dados:
     
     def __init__(self, json_list): 
+        """
+        Inicializa a classe com a lista de JSONs e a conexão com o DuckDB.
+
+        :param json_list: Lista de dados no formato JSON para serem carregados.
+        """
         self.json_list = json_list 
         self.con = db.connect()
 
     def load_data(self):
+        """
+        Carrega os dados da lista de JSONs para o DuckDB, criando tabelas a partir de cada elemento da lista.
+        Cada JSON é convertido em um DataFrame e registrado no banco de dados.
+        """
         for i, data in enumerate(self.json_list):
             df = pd.DataFrame(data)
             nome_tabela =f"table_{i}"
@@ -19,6 +28,19 @@ class Tratamento_dados:
             logging.info(f"Tabela '{nome_tabela}' carregada no DuckDB.")
 
     def query_data(self, sql_query):
-        logging.info("Iniciando a execução da consulta SQL.")
-        result_df = self.con.execute(f"COPY ({sql_query}) TO 'arquivo_final.json' (FORMAT 'json')")
-        logging.info("Consulta SQL executada e arquivo JSON exportado com sucesso.")
+        """
+        Executa uma consulta SQL no DuckDB e exporta o resultado para um arquivo JSON.
+
+        :param sql_query: A consulta SQL que será executada.
+        """
+        try:
+            logging.info("Iniciando a execução da consulta SQL.")
+            self.con.execute(f"COPY ({sql_query}) TO 'arquivo_final.json' (FORMAT 'json')")
+            logging.info("Consulta SQL executada e arquivo JSON exportado com sucesso.")
+        except Exception as e:
+            logging.error(f"Erro ao executar a consulta SQL: {e}")
+            return None  # Retorna None em caso de erro
+        finally:
+            # Fechar a conexão após a execução para liberar recursos
+            self.con.close()
+            logging.info("Conexão com o DuckDB fechada.")
